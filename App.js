69576e691e6c8e5947ef0887c5aae3474fb504c4
Breno Padovan <brenohpadovan@gmail.com>
2023-10-20 23:21:55 -0300

    Feito atualizações na interface, finalização do slider de caracteres

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
-import {View, Text, StyleSheet, Image} from 'react-native'
+import { useState } from 'react'
+import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native'
 import Slider from '@react-native-community/slider'
 
 
 export default function App(){
+  const [size, setSize] = useState(10) 
+
+
+
+
   return(
     <View style={styles.container}>
       <Image
@@ -10,15 +16,24 @@ export default function App(){
         style={styles.logo}
       />
 
-      <Text style={styles.title}>20 caracteres</Text>
+      <Text style={styles.title}>{size} caracteres</Text>
 
       <View style={styles.area}>
         <Slider
         style={{height: 50}}
         minimumValue={6}
         maximumValue={20}
+        maximumTrackTintColor='#ff0000'
+        minimumTrackTintColor='#000'
+        thumbTintColor='#392de9'
+        value={size}
+        onValueChange={ (value) => setSize(value.toFixed(0)) }
         />
       </View>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Gerar senha</Text>
+      </TouchableOpacity>
 
 
     </View>
@@ -44,5 +59,24 @@ const styles = StyleSheet.create({
     backgroundColor: "#FFF",
     borderRadius: 8,
     padding: 6,
+  },
+
+  button:{
+    backgroundColor: "#392de9",
+    width: "80%",
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8,
+    marginBottom: 18
+  },
+  buttonText:{
+    color: "#FFF",
+    fontSize: 20,
+
+  },
+  title:{
+    fontSize: 30,
+    fontWeight: "bold"
   }
 })
