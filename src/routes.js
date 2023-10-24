3c51d4a721d4715c6691ee3c49e4d08435ee3cb2
Breno Padovan <brenohpadovan@gmail.com>
2023-10-24 00:00:54 -0300

    Update navigation

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Home } from './pages/home'
 import { Passwords } from './pages/passwords'
 
+import {Ionicons} from '@expo/vector-icons'
 
 const Tab = createBottomTabNavigator();
 
@@ -12,11 +13,31 @@ export function Routes(){
             <Tab.Screen
                 name="home"
                 component={Home}
+                options={{
+                    tabBarShowLabel: false,
+                    headerShown: false,
+                    tabBarIcon: ({focused, size, color}) => {
+                        if(focused){
+                            return <Ionicons size={size} color={color} name="home"/>
+                        }
+                        return <Ionicons size={size} color={color} name="home-outline"/>
+                    }
+                }}
             />
 
             <Tab.Screen
-            name="passwords"
-            component={Passwords}
+                name="passwords"
+                component={Passwords}
+                options={{
+                tabBarShowLabel: false,
+                headerShown: false,
+                tabBarIcon: ({focused, size, color}) => {
+                    if(focused){
+                        return <Ionicons size={size} color={color} name="lock-closed" />
+                    }
+                    return <Ionicons size={size} color={color} name="lock-closed-outline"/>
+                }
+            }}
             />
 
         </Tab.Navigator>
